6c713967b452f38bc72a46ac7a73c84879f0beb9
Alessandro Parma <alessa.parma@gmail.com>
2018-01-10 18:00:59 +0100

    improve startup wrapper
    if custom named GeoServer WAR does not exist look for geoserver.war

--- catalina-wrapper.sh
+++ catalina-wrapper.sh
@@ -10,15 +10,18 @@ webapp_path="${CATALINA_BASE}/webapps/${GEOSERVER_APP_NAME}"
 # not deployed? first run? unpack GeoServer WAR
 if [ ! -d "$webapp_path" ]; then
     echo "GeoServer web application not found, unpacking WAR"
-    if [ ! -f "${webapp_path}.war" ]; then
+    if [ -f "${webapp_path}.war" ];then
+      war_path="${webapp_path}.war"
+    elif [ -f "${CATALINA_BASE}/webapps/geoserver.war" ]; then
+      war_path="${CATALINA_BASE}/webapps/geoserver.war"
+    else
       echo "ERROR: ${webapp_path}.war not found!, exiting"
       exit 2
-    else
-      mkdir "$webapp_path" && unzip -q -o "${webapp_path}.war" -d "$webapp_path"
-      if [ $? -ne 0 ]; then
-        echo "ERROR: GeoServer WAR extraction failed!"
-        exit 3
-      fi
+    fi
+    mkdir "$webapp_path" && unzip -q -o "$war_path" -d "$webapp_path" && rm -f "$war_path"
+    if [ $? -ne 0 ]; then
+      echo "ERROR: GeoServer WAR extraction failed!"
+      exit 3
     fi
 fi
 
