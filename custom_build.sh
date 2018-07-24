267c0b7081f8588ccea58caeff3503a03e0524a9
Nicolino Curalli <nicolino.curalli@gmail.com>
2018-07-24 11:21:00 +0200

    custom build script to burn in docker a tagged version of datadir, a released version of geoserver war and plugins (#5)
    * added scritp to build a geoserver docker with all burned in itself : need to implement datadir burning from github release artefact
    
    * custom build with all burned works
    
    * added support for dev environment for datadir  : more flexible container with datadir not burned in container by GEOSERVER_DATA_DIR_RELEASE = dev
    
    * clean up of previous commit
    
    * fixing
    
    * fixed geoserver unpackaging before burning
    
    * added parameter to force pull of base docker from dockerhub
    
    * Remove commented sections

---
+++ custom_build.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+set -x
+set -e
+TAG=${1}
+readonly GEOSERVER_VERSION=${2}
+readonly GEOSERVER_MASTER_VERSION=${3}
+readonly GITHUB_TOKEN=${4}
+readonly GITHUB_REPO=${5} 
+readonly GITHUB_REPO_OWNER=${6} 
+readonly GEOSERVER_DATA_DIR_RELEASE=${7}
+readonly PULL=${8}
+
+
+
+readonly BASE_BUILD_URL="https://build.geoserver.org/geoserver/"
+readonly ARTIFACT_DIRECTORY=./resources
+readonly GEOSERVER_ARTIFACT_DIRECTORY=${ARTIFACT_DIRECTORY}/geoserver/
+readonly DATADIR_ARTIFACT_DIRECTORY=${ARTIFACT_DIRECTORY}/geoserver-datadir/
+readonly PLUGIN_ARTIFACT_DIRECTORY=${ARTIFACT_DIRECTORY}/geoserver-plugins/
+
+
+function clean_up_directory() {
+	rm -rf ${1}/*
+}
+
+function download_from_url_to_a_filepath {
+	URL=${1}
+	FILE_PATH=${2}
+	FILE_DOWNLOADED=$(basename "${FILE_PATH}" )
+	if [ ! -f "${FILE_PATH}" ]; then
+		curl -L "${URL}" --output "${FILE_PATH}"
+		echo "* ${FILE_DOWNLOADED} artefact dowloaded *"
+	else
+		echo "* ${FILE_DOWNLOADED} artefact already dowloaded *"
+	fi
+}
+
+function get_release_artifact_url_from_github() {
+	REPO=${1}
+    OWNER=${2}
+    RELEASE=${3}
+    local TEMP_FILE_PATH=/tmp/${RELEASE}
+	GH_API="https://api.github.com"
+	GH_REPO="$GH_API/repos/${OWNER}/${REPO}"
+    GH_TARBALL="${GH_REPO}/tarball"
+	declare -a HEADERS=("-H \"Authorization: token ${GITHUB_TOKEN}\"" '-H "Accept: application/vnd.github.v3.raw"')
+    ENDPOINT="${GH_TARBALL}"
+
+    PRE_ARTIFACT_URL="curl -L ${HEADERS[@]} -s ${ENDPOINT} --output ${TEMP_FILE_PATH}"
+    RELEASE_ITEMS=$(eval $PRE_ARTIFACT_URL)
+    tar xzvf "${TEMP_FILE_PATH}" --strip=1 -C "${DATADIR_ARTIFACT_DIRECTORY}"
+}
+ 
+function download_plugin()  {
+	TYPE=${1}
+	PLUGIN_NAME=${2}
+
+	if  [[ "${GEOSERVER_VERSION}" == "master" ]]; then
+		PLUGIN_FULL_NAME=geoserver-${GEOSERVER_MASTER_VERSION::-2}-SNAPSHOT-${PLUGIN_NAME}-plugin.zip
+		local PLUGIN_ARTIFACT_URL=${BASE_BUILD_URL}/${GEOSERVER_VERSION}/${TYPE}-latest/${PLUGIN_FULL_NAME}
+ 
+	else
+		PLUGIN_FULL_NAME=geoserver-${GEOSERVER_VERSION::-2}-SNAPSHOT-${PLUGIN_NAME}-plugin.zip
+		local PLUGIN_ARTIFACT_URL=${BASE_BUILD_URL}/${GEOSERVER_VERSION}/${TYPE}-latest/${PLUGIN_FULL_NAME}
+
+	fi
+
+    download_from_url_to_a_filepath "${PLUGIN_ARTIFACT_URL}" "${PLUGIN_ARTIFACT_DIRECTORY}${PLUGIN_FULL_NAME}"
+}
+
+function download_geoserver() {
+	clean_up_directory ${GEOSERVER_ARTIFACT_DIRECTORY}
+	local VERSION=${1}
+	local GEOSERVER_FILE_NAME="geoserver-${VERSION}-latest-war.zip"
+	local GEOSERVER_ARTIFACT_URL=${BASE_BUILD_URL}/${VERSION}/${GEOSERVER_FILE_NAME}
+	download_from_url_to_a_filepath  "${GEOSERVER_ARTIFACT_URL}" "/tmp/geoserver.war.zip"
+    unzip -p /tmp/geoserver.war.zip geoserver.war > ${GEOSERVER_ARTIFACT_DIRECTORY}/geoserver.war
+}
+
+
+function build_with_data_dir() {
+
+	local TAG=${1}
+
+	docker build --no-cache \
+		--build-arg BASE_IMAGE_NAME=gs-base \
+		--build-arg BASE_IMAGE_TAG=7.0-jre8 \
+		--build-arg INCLUDE_DATA_DIR=true \
+		--build-arg INCLUDE_GS_WAR=true \
+		--build-arg INCLUDE_PLUGINS=true \
+		--build-arg GEOSERVER_APP_NAME=geoserver \
+		-t geosolutionsit/geoserver:maps-"${TAG}" \
+		 .
+}
+
+function build_without_data_dir() {
+
+	local TAG=${1}
+	local PULL_ENABLED=${2}
+	if [[ "${PULL_ENABLED}" == "pull" ]]; then
+		DOCKER_BUILD_COMMAND="docker build --pull"
+	else
+		DOCKER_BUILD_COMMAND="docker build"
+	fi;	
+	${DOCKER_BUILD_COMMAND} --no-cache \
+		--build-arg BASE_IMAGE_NAME=gs-base \
+		--build-arg BASE_IMAGE_TAG=7.0-jre8 \
+		--build-arg INCLUDE_DATA_DIR=false \
+		--build-arg INCLUDE_GS_WAR=true \
+		--build-arg INCLUDE_PLUGINS=true \
+		--build-arg GEOSERVER_APP_NAME=geoserver \
+		-t geosolutionsit/geoserver:maps-"${TAG}"-dev \
+		 .
+}
+
+
+
+function main {
+    
+	download_geoserver "${GEOSERVER_VERSION}"
+	clean_up_directory ${PLUGIN_ARTIFACT_DIRECTORY}
+	download_plugin ext feature-pregeneralized 
+	download_plugin ext css
+	if  [[ ${GEOSERVER_DATA_DIR_RELEASE} = "dev" ]]; then
+   	    build_without_data_dir "${TAG}" "${PULL}"
+   	else
+   		clean_up_directory ${DATADIR_ARTIFACT_DIRECTORY}
+		get_release_artifact_url_from_github "${GITHUB_REPO}" "${GITHUB_REPO_OWNER}" "${GEOSERVER_DATA_DIR_RELEASE}"
+ 		build_with_data_dir "${TAG}" "${PULL}"
+   	fi
+}
+
+main
