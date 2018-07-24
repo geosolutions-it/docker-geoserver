ddad44e46a19b5cee4c41eefcfd5e8a339d008e5
Alessandro Parma <randomorder@users.noreply.github.com>
2018-07-24 11:20:21 +0200

    Remove commented sections

--- custom_build.sh
+++ custom_build.sh
@@ -36,12 +36,6 @@ function download_from_url_to_a_filepath {
 	fi
 }
 
-#84b846c95ffac2ef93c635e0657c9d46eead4ed1
-#https://codeload.github.com/geosolutions-it/anaximander/tar.gz/0.1?token=Ai5866Ah_GED_tUtRbtf7NLLSGN6HMI0ks5bUI8FwA==
-#curl -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw"  -s https://api.github.com/repos/$REPO/releases | jq ". | map(select(.tag_name == \"$VERSION\"))[0].tarball_url"
-#curl -L -H "Authorization: token $GITHUB_TOKEN"  https://api.github.com/repos/geosolutions-it/anaximander/tarball/0.1 -o /tmp/pippo
-
-
 function get_release_artifact_url_from_github() {
 	REPO=${1}
     OWNER=${2}
@@ -56,35 +50,8 @@ function get_release_artifact_url_from_github() {
     PRE_ARTIFACT_URL="curl -L ${HEADERS[@]} -s ${ENDPOINT} --output ${TEMP_FILE_PATH}"
     RELEASE_ITEMS=$(eval $PRE_ARTIFACT_URL)
     tar xzvf "${TEMP_FILE_PATH}" --strip=1 -C "${DATADIR_ARTIFACT_DIRECTORY}"
-
-
- #    declare -a filter=("| jq \"." "| map(select(.tag_name == $RELEASE))[0].tarball_url\"" )
- #    echo "${filter[@]}"
- #    ARTIFACT_URL="echo \"${RELEASE_ITEMS}\" ${filter[@]}"
- #    echo -e
-	# echo $ARTIFACT_URL
-	# echo -e
- #   	eval $ARTIFACT_URL
-
-   # echo $ciccio
-   # echo $ciccio | jq ". | map(select(.tag_name == \"$RELEASE\"))[0].tarball_url"
-   #ARFIFACT_URL=$(curl "${HEADERS}" -s "${ENDPOINT}"| jq ". | map(select(.tag_name == \"${RELEASE}\"))[0].tarball_url") 
-
 }
  
-
-
-# function get_datadir() {
-# 	# local GEOSERVER_DATA_DIR_REPOSITORY=${1}
-#  #    local GEOSERVER_DATA_DIR_RELEASE=${2}
-#     local TEMP_FILE_PATH=/tmp/${GEOSERVER_DATA_DIR_RELEASE}
-# 	local URL="${GEOSERVER_DATA_DIR_REPOSITORY}"/"${GEOSERVER_DATA_DIR_RELEASE}"?token="${GITHUB_TOKEN}"
-# 	echo $URL
-# 	download_from_url_to_a_filepath "${URL}" "${TEMP_FILE_PATH}"
-# 	tar xzvf "${TEMP_FILE_PATH}" --strip=1 -C "${DATADIR_ARTIFACT_DIRECTORY}"
-# }
-
-
 function download_plugin()  {
 	TYPE=${1}
 	PLUGIN_NAME=${2}
