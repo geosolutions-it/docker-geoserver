1b10b6aa97a169910a734f1ce545bd8a0e444310
GeoSolutions <geosolutions@maps.geo-solutions.it>
2018-09-13 10:05:10 +0000

    ass vectortiles plugin

--- custom_build.sh
+++ custom_build.sh
@@ -193,6 +193,7 @@ function main {
     download_plugin ext monitor
     download_plugin ext control-flow
     download_plugin ext libjpeg-turbo
+    download_plugin ext vectortiles
     download_plugin community status-monitoring
     download_plugin community qos
     download_marlin
