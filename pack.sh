0608a8a79c8c3841bcf5771ab859520843ede59f
Alessandro Parma <randomorder@users.noreply.github.com>
2019-05-06 12:35:37 +0200

    Update pack.sh
    Add CSS plugin

--- pack.sh
+++ pack.sh
@@ -156,6 +156,7 @@ function main {
     download_plugin ext monitor
     download_plugin ext querylayer
     download_plugin ext wps
+    download_plugin ext css
     download_plugin community authkey
     download_plugin community status-monitoring
     download_plugin community wmts-multi-dimensional
