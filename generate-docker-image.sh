cf33bab59d11b2a47b14202bc4a420dbfac277ce
Jose Macchi <jose.macchi@geo-solutions.it>
2020-12-11 12:55:02 -0300

    Improvements. Including catalina-wrapper features.

--- generate-docker-image.sh
+++ generate-docker-image.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # You should modify parameters (8) to consider your own custom build
-./custom_build.sh gs-2.18.1 2.18.x 2.18.1 dummy dummy dummy dev no-pull
+# TODO: work on custom_build for releases that are not SNAPSHOTS -> ./custom_build.sh gs-2.18.1 2.18.x 2.18.1 dummy dummy dummy dev no-pull
+# instead doing it manually
+docker build --build-arg=GEOSERVER_WEBAPP_SRC=./geoserver.war . -t gs_unavco
