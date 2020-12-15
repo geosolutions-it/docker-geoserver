2e8972daf6873e38c9086958b7bcd4366ab45f05
Jose Macchi <jemacchi@yahoo.com.ar>
2020-12-15 09:39:02 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker-compose -f ./docker-compose.yml up -d --scale geoserver-slave=2
+#docker-compose -f ./docker-compose.yml up -d --scale geoserver-slave=2
+docker-compose -f ./docker-compose.yml up -d
