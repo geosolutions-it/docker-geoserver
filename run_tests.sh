8a7e4f5895d993a0b80d20bfd34911778ca9417e
Luca Pasquali <luca@bucaniere.us>
2020-12-17 16:58:50 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
 CURL=$(which curl) 
-catalina.sh run &
-printenv
-sleep 15
-ls -la webapps/geoserver/
-cat $GEOSERVER_LOG_DIR/geoserver.log
+catalina.sh run 2>&1 > /dev/null &
 $CURL --retry-delay 1 --retry 60 -X GET http://$HOSTNAME:8080/geoserver/rest/about/manifest -H  "accept: application/json" -H  "content-type: application/json"
+$CURL --retry-delay 1 --retry 60 -X GET http://localhost:8080/geoserver/rest/layers -H  "accept: application/json" -H  "content-type: application/json"
+$CURL --retry-delay 1 --retry 60 -X GET 'http://localhost:8080/geoserver/wms?service=wms&version=1.1.1&request=GetCapabilities'
+cat $GEOSERVER_LOG_DIR/geoserver.log
