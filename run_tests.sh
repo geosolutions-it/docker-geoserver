64fae4393a04fa7a3c1e4dc2ae4aa1cfdf6950db
Luca Pasquali <luca@bucaniere.us>
2020-12-17 15:55:35 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,8 @@
 CURL=$(which curl) 
 catalina.sh run &
 printenv
-sleep 5
-$CURL --retry 60 -u admin:geoserver "http://$HOSTNAME:8080/geoserver/gwc/rest/layers"
+sleep 15
+ls -la webapps/geoserver/
+$CURL --retry-delay 1 --retry 60 -X GET http://$HOSTNAME:8080/geoserver/rest/about/manifest -H  "accept: application/json" -H  "content-type: application/json"
 
 netstat -lutanp
