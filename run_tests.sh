e55074339d75954b0815947e3f67380a7d1ec9d6
Luca Pasquali <luca@bucaniere.us>
2020-12-17 15:56:33 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -6,5 +6,3 @@ printenv
 sleep 15
 ls -la webapps/geoserver/
 $CURL --retry-delay 1 --retry 60 -X GET http://$HOSTNAME:8080/geoserver/rest/about/manifest -H  "accept: application/json" -H  "content-type: application/json"
-
-netstat -lutanp
