7070c428949c3ebae4fda6384a48a7b542177fde
Luca Pasquali <luca@bucaniere.us>
2020-12-17 14:43:02 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,6 @@ CURL=$(which curl)
 
 printenv
 sleep 30
-$CURL -u admin:geoserver "http://$HOSTNAME:8080/geoserver/gwc/rest/layers"
+#$CURL -u admin:geoserver "http://$HOSTNAME:8080/geoserver/gwc/rest/layers"
 
 
