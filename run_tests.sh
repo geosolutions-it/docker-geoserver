d6640adc1672b1659af007b2238c212ce3c4bfb1
Luca Pasquali <luca@bucaniere.us>
2020-12-17 17:06:39 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 CURL=$(which curl) 
-catalina.sh run 2>&1 > /dev/null &
+catalina.sh run &
 sleep 30
 $CURL --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/rest/about/manifest" -H "accept: application/json" -H "content-type: application/json"
 $CURL --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/rest/layers" -H "accept: application/json" -H "content-type: application/json"
