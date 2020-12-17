bc0438d84b25b33c1c960ad7b75b0bb7ae58ac96
Luca Pasquali <luca@bucaniere.us>
2020-12-17 13:20:06 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,6 @@ CURL=$(which curl)
 
 printenv
 
-$CURL --retry 60 ---connect-timeout 5 -retry-delay 1 -u admin:geoserver "http://$HOSTNAME:8080/geoserver/gwc/rest/layers"
+$CURL --retry 60 --connect-timeout 5 -retry-delay 1 -u admin:geoserver "http://$HOSTNAME:8080/geoserver/gwc/rest/layers"
 
 
