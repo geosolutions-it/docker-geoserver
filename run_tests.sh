ea9c2cba8a9f1ee47141f3cc20e6416cea34fb75
Luca Pasquali <luca@bucaniere.us>
2020-12-17 15:20:28 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 CURL=$(which curl) 
-
+catalina.sh run &
 printenv
-#sleep 30
-#$CURL -u admin:geoserver "http://$HOSTNAME:8080/geoserver/gwc/rest/layers"
+sleep 5
+$CURL --retry 60 -u admin:geoserver "http://$HOSTNAME:8080/geoserver/gwc/rest/layers"
 
 netstat -lutanp
