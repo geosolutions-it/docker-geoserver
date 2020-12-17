f25d23b87993ee0201a50f65b18d96e80125a1c6
Luca Pasquali <luca@bucaniere.us>
2020-12-17 19:02:13 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,4 @@ catalina.sh start
 sleep 4
 #$CURL --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/rest/about/manifest" -H "accept: application/json" -H "content-type: application/json"
 #$CURL --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/rest/layers" -H "accept: application/json" -H "content-type: application/json"
-$CURL -s --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/wms?service=wms&version=1.1.1&request=GetCapabilities"
+$CURL -o /dev/null -s --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/wms?service=wms&version=1.1.1&request=GetCapabilities"
