7f176f9ebf513b51ebe108c6cb15ec67d37f9368
Luca Pasquali <luca@bucaniere.us>
2020-12-17 12:03:18 +0100

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,9 @@
 
 CURL=$(which curl) 
 
+printenv
+
 $CURL --retry 60 --retry-delay 1 --fail \
--u admin:geoserver "http://localhost:8080/geoserver/gwc/rest/layers" || ( echo "test failed" && exit 2 )
+-u admin:geoserver "http://sut:8080/geoserver/gwc/rest/layers" || ( echo "test failed" && exit 2 )
+
 
