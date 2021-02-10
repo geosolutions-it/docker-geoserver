1ef56203041722735df7b6b5bd27de313d0c75ad
Asdrubal Gonzalez <agpenton@gmail.com>
2021-02-10 16:48:10 +0400

    docker-compose added: - proxy service added. - nginx config for geoserver added. - postgres service added. - postgres ENV variables added. - database initialization added.

---
+++ postgres/01-init-user.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username ${POSTGRES_USER} --dbname ${POSTGRES_DB} <<-EOSQL
+    CREATE USER geoserver;
+    CREATE DATABASE geoserver;
+    GRANT ALL PRIVILEGES ON DATABASE geoserver TO geoserver;
+    CREATE DATABASE "gwc_quota" WITH OWNER "geoserver";
+EOSQL
