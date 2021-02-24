8d256126b1338f7fe058a375a89658793f0bad3d
Asdrubal Gonzalez <agpenton@gmail.com>
2021-02-24 14:53:04 +0400

    changing syntax on custom_build.sh

--- custom_build.sh
+++ custom_build.sh
@@ -178,12 +178,10 @@ function main {
     download_geoserver "${GEOSERVER_VERSION}"
     clean_up_directory ${PLUGIN_ARTIFACT_DIRECTORY}
     create_plugins_folder
-    download_plugin ext feature-pregeneralized
-    download_plugin ext css
     download_plugin ext monitor
     download_plugin ext control-flow
-    download_plugin ext libjpeg-turbo
-    download_marlin
+    download_plugin community sec-oauth2-geonode
+    #download_marlin
 
 	if  [[ ${GEOSERVER_DATA_DIR_RELEASE} = "nodatadir" ]]; then
    	    build_without_data_dir "${TAG}" "${PULL}"
