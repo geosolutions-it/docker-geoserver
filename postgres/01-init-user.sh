329db3edae5f726910e2dc637206841b6e51af9a
Asdrubal Gonzalez <agpenton@gmail.com>
2021-05-31 11:44:20 +0400

    adding changes requested

--- postgres/01-init-user.sh
+++ postgres/01-init-user.sh
@@ -4,6 +4,8 @@ set -e
 psql -v ON_ERROR_STOP=1 --username ${POSTGRES_USER} --dbname ${POSTGRES_DB} <<-EOSQL
     CREATE USER geoserver;
     CREATE DATABASE geoserver;
+    create EXTENSION postgis;
     GRANT ALL PRIVILEGES ON DATABASE geoserver TO geoserver;
     CREATE DATABASE "gwc_quota" WITH OWNER "geoserver";
+    CREATE EXTENSION postgis;
 EOSQL
