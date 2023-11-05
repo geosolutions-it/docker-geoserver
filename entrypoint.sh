5bfbcd945799a4317f717f83e5ae55b7d3af15ef
Francesco Camuffo <dev@fmac.xyz>
2023-11-05 17:45:47 +0100

    entrypoint.sh: wait for the DB service

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 geoserver-plugin-download.sh ${CATALINA_BASE}/webapps/${APP_LOCATION}/WEB-INF/lib $PLUGIN_DYNAMIC_URLS
 set -m
+
+# FIXME: this workaround (for a deploy in a Swarm) keeps GS from starting and
+# crashing, because of JDBC not being able to connect to Postgres
+db_err=6
+while [ $db_err -eq 6 ]; do
+  curl -sS "$DATABASE_HOST"
+  db_err=$?
+  sleep 1
+done
+
 catalina.sh run &
 /usr/local/bin/geoserver-rest-config.sh
 fg %1
