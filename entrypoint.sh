0c8bf7e945ed7255c392ff968b50f6e7e3047101
Francesco Camuffo <68204638+camuffo@users.noreply.github.com>
2023-11-06 11:58:12 +0100

    C268 (#130)
    * Dockerfile: avoid the unreliable SourceForge
    
    * Build with project-specific plugins
    
    * entrypoint.sh: wait for the DB service
    
    * entrypoint.sh: check for DB with pg_isready
    
    * entrypoint.sh: check $DATABASE_HOST

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 geoserver-plugin-download.sh ${CATALINA_BASE}/webapps/${APP_LOCATION}/WEB-INF/lib $PLUGIN_DYNAMIC_URLS
 set -m
+
+# FIXME: this workaround (for a deploy in a Swarm) keeps GS from starting and
+# crashing, because of JDBC not being able to connect to Postgres
+attempts=0
+while ! /usr/bin/pg_isready -h "$DATABASE_HOST"; do
+  attempts=$((attempts + 1))
+  printf "Checking database availability: attempt %s\n" "$attempts"
+  sleep 1
+done
+
 catalina.sh run &
 /usr/local/bin/geoserver-rest-config.sh
 fg %1
