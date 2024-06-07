86bcbe8c158753a0e37a83219362823b3f63cc2c
Alessandro Parma <alessa.parma@gmail.com>
2024-06-07 17:55:19 +0200

    Update run_tests.sh
    remove unwanted jars

--- run_tests.sh
+++ run_tests.sh
@@ -6,3 +6,13 @@ sleep 4
 #$CURL --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/rest/about/manifest" -H "accept: application/json" -H "content-type: application/json"
 #$CURL --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/rest/layers" -H "accept: application/json" -H "content-type: application/json"
 $CURL -I -s --retry-delay 1 --retry 60 -X GET "http://$HOSTNAME:8080/geoserver/wms?service=wms&version=1.1.1&request=GetCapabilities"
+
+# check files are not present
+file1="${CATALINA_BASE}/webapps/geoserver/WEB-INF/lib/gt-complex-*.jar"
+file2="${CATALINA_BASE}/webapps/geoserver/WEB-INF/lib/commons-jxpath-*.jar"
+if [[ ! -e $file1 && ! -e $file2 ]]; then
+  echo "Both files are not present."
+else
+  echo "At least one file is present."
+  exit 2
+fi
