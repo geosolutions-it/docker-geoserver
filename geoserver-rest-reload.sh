07227270027bc5a80920bf27621eabb8d1f2c23b
Francesco Camuffo <68204638+camuffo@users.noreply.github.com>
2024-12-12 12:03:34 +0100

    Support APP_LOCATION at runtime (#166)

--- geoserver-rest-reload.sh
+++ geoserver-rest-reload.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -x
+
+if [ -z "${APP_LOCATION}" ]; then
+    APP_LOCATION="geoserver"
+fi
+
 #while [ "$(curl -s --retry-connrefused --retry 100 -I http://localhost:8080/geoserver/web/ 2>&1 |grep 200)" == "" ];do
 #  echo "Waiting for GeoServer to be Up and running"
-#done  
+#done
 if [ "$ADMIN_PASSWORD" != "" ]; then
-    ADMIN_HEADER=$(echo -n "admin:${ADMIN_PASSWORD}" | base64)    
+    ADMIN_HEADER=$(echo -n "admin:${ADMIN_PASSWORD}" | base64)
 else
     ADMIN_HEADER=$(echo -n "admin:geoserver"| base64)
-fi 
+fi
 
-curl -H "Authorization: basic ${ADMIN_HEADER}" -X POST http://localhost:8080/geoserver/rest/reload -H  "accept: application/json" -H  "content-type: application/json"
+curl -H "Authorization: basic ${ADMIN_HEADER}" -X POST http://localhost:8080/"$APP_LOCATION"/rest/reload -H  "accept: application/json" -H  "content-type: application/json"
