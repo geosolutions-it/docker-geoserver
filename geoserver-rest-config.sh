fb6a5197b19c60cf9a4b83b440281e2110a021a0
Francesco Camuffo <68204638+camuffo@users.noreply.github.com>
2024-12-12 12:03:34 +0100

    Support APP_LOCATION at runtime (#166)

--- geoserver-rest-config.sh
+++ geoserver-rest-config.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-while [ "$(curl -s --retry-connrefused --retry 100 -I http://localhost:8080/geoserver/web/ 2>&1 |grep 200)" == "" ];do
-  echo "Waiting for GeoServer to be Up and running"
-done  
+
+if [ -z "${APP_LOCATION}" ]; then
+    APP_LOCATION="geoserver"
+fi
+
+while [ "$(curl -s --retry-connrefused --retry 100 -I http://localhost:8080/"$APP_LOCATION"/web/ 2>&1 |grep 200)" == "" ];do
+    echo "Waiting for GeoServer to be Up and running"
+done
 if [ "$ADMIN_PASSWORD" != "" ]; then
     echo "GeoServer password is likely to be default, going to change to new admin password."
     ADMIN_HEADER=$(echo -n "admin:geoserver" | base64)
-    curl -H "Authorization: basic $ADMIN_HEADER" -X PUT http://localhost:8080/geoserver/rest/security/self/password -H  "accept: application/json" -H  "content-type: application/json" -d "{  \"newPassword\": \"$ADMIN_PASSWORD\"}"
-fi  
+    curl -H "Authorization: basic $ADMIN_HEADER" -X PUT http://localhost:8080/"$APP_LOCATION"/rest/security/self/password -H  "accept: application/json" -H  "content-type: application/json" -d "{  \"newPassword\": \"$ADMIN_PASSWORD\"}"
+fi
