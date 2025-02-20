71d04ecb07ed5f899ef8c2c9d41cecf15bee64af
Jannis R <mail@jannisr.de>
2025-02-21 00:32:35 +0100

    chore: geoserver-plugin-download.sh: refactor: use if instead of case statement

--- geoserver-plugin-download.sh
+++ geoserver-plugin-download.sh
@@ -9,10 +9,12 @@ PLUGIN_INSTALL_PATH=$1
 
 for url in "${@:2}"
 do
-    case "$url" in
-        *sourceforge*) wget "$url/download" && unzip -o ./download -d ${PLUGIN_INSTALL_PATH} && rm ./download
-        ;;
-        *) wget -O ./download "$url" && unzip -o ./download -d ${PLUGIN_INSTALL_PATH} && rm ./download
-        ;;
-    esac
+    # support specifying SourceForge URLs without the `/download` part at the end necessary for downloading
+    if [[ "$url" == *sourceforge* ]]; then
+        url="$url/download"
+    fi
+
+    wget -O ./download "$url"
+    unzip -o ./download -d ${PLUGIN_INSTALL_PATH}
+    rm ./download
 done
