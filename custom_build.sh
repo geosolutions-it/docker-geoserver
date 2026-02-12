0081b1d0874eba64b756c8c293b09e4083aa1f24
Brian <213060881+brynsofz@users.noreply.github.com>
2026-02-12 20:01:43 +0700

    fix: update to build GeoServer 3.0-nightly image

--- custom_build.sh
+++ custom_build.sh
@@ -9,8 +9,8 @@ readonly GEOSERVER_DATA_DIR_RELEASE=${4}
 readonly PULL=${5}
 readonly ALL_PARAMETERS=$*
 readonly BASE_BUILD_URL="https://build.geoserver.org/geoserver"
-readonly BASE_BUILD_URL_STABLE="https://netcologne.dl.sourceforge.net/project/geoserver/GeoServer"
-#readonly BASE_BUILD_URL_STABLE="https://build.geoserver.org/geoserver"
+# readonly BASE_BUILD_URL_STABLE="https://netcologne.dl.sourceforge.net/project/geoserver/GeoServer"
+readonly BASE_BUILD_URL_STABLE="https://build.geoserver.org/geoserver"
 readonly EXTRA_FONTS_URL="https://www.dropbox.com/s/hs5743lwf1rktws/fonts.tar.gz?dl=1"
 readonly MARLIN_VERSION=0.9.2
 readonly ARTIFACT_DIRECTORY=./resources
@@ -72,15 +72,8 @@ function download_plugin()  {
 		;;
 
 		*)
-		PLUGIN_FULL_NAME=geoserver-${GEOSERVER_VERSION}-${PLUGIN_NAME}-plugin.zip
-		if [ "${TYPE}" == "ext" ]; then
-			NEWTYPE=extensions
-			PLUGIN_ARTIFACT_URL=${BASE_BUILD_URL_STABLE}/${GEOSERVER_VERSION}/${NEWTYPE}/${PLUGIN_FULL_NAME}
-		else
-			VERSION="${GEOSERVER_VERSION%.*}-SNAPSHOT"
-			PLUGIN_FULL_NAME=geoserver-${VERSION}-${PLUGIN_NAME}-plugin.zip
-			PLUGIN_ARTIFACT_URL=${BASE_BUILD_URL}/${GEOSERVER_VERSION%.*}.x/${TYPE}-latest/${PLUGIN_FULL_NAME}
-		fi
+		PLUGIN_FULL_NAME=geoserver-${GEOSERVER_VERSION}-SNAPSHOT-${PLUGIN_NAME}-plugin.zip
+		PLUGIN_ARTIFACT_URL=${BASE_BUILD_URL}/main/${TYPE}-latest/${PLUGIN_FULL_NAME}
 		;;
 	esac
   download_from_url_to_a_filepath "${PLUGIN_ARTIFACT_URL}" "${PLUGIN_ARTIFACT_DIRECTORY}/${PLUGIN_FULL_NAME}"
@@ -119,6 +112,9 @@ function download_geoserver() {
 
 		if [[ ( "${VERSION}" =~ "x" ) || ( "${VERSION}" == "main" ) ]]; then
 			local GEOSERVER_ARTIFACT_URL=${BASE_BUILD_URL}/${VERSION}/${GEOSERVER_FILE_NAME_NIGHTLY}
+		elif [[ "${VERSION}" == "${GEOSERVER_MASTER_VERSION}" ]]; then
+			local GEOSERVER_FILE_NAME_MAIN="geoserver-main-latest-war.zip"
+			local GEOSERVER_ARTIFACT_URL=${BASE_BUILD_URL}/main/${GEOSERVER_FILE_NAME_MAIN}
 		else
 			local GEOSERVER_ARTIFACT_URL=${BASE_BUILD_URL_STABLE}/${VERSION}/${GEOSERVER_FILE_NAME_STABLE}
 		fi
@@ -222,11 +218,15 @@ function main {
     clean_up_directory 
     download_geoserver "${GEOSERVER_VERSION}"
     create_plugins_folder
-    # download_plugin ext monitor
-    # download_plugin ext control-flow
-    # download_plugin ext geofence-plugin
-    # download_plugin ext geofence-server-plugin
-    # download_plugin community sec-oauth2-geonode
+    download_plugin ext monitor
+    download_plugin ext control-flow
+    download_plugin ext wps
+    download_plugin ext vectortiles
+    download_plugin community ogcapi-processes
+    download_plugin community ogcapi-coverages
+    download_plugin community ogcapi-maps
+    download_plugin community ogcapi-tiles
+
     #download_marlin
 
 	if  [ "${GEOSERVER_DATA_DIR_RELEASE}" = "nodatadir" ]; then
