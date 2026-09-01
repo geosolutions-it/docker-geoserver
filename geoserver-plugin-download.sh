2739b21c4482d01153ae25fa39f9988c047061b6
mahesh-wor <maheshwor89@gmail.com>
2026-09-01 15:24:46 +0545

    fix: fail image builds when plugin downloads or extraction fail & lint

--- geoserver-plugin-download.sh
+++ geoserver-plugin-download.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
-[ "$#" -le "1" ] && ( echo "no plugin urls passed, exiting" ) && exit 0
+set -euo pipefail
+
+if [ "$#" -le 1 ]; then
+    echo "no plugin urls passed, exiting"
+    exit 0
+fi
 
 PLUGIN_INSTALL_PATH=$1
+shift
 
-for url in "$@"
-do
+for url in "$@"; do
     case "$url" in
-        *sourceforge*) wget "$url/download" && unzip -o ./download -d ${PLUGIN_INSTALL_PATH} && rm ./download
+        *sourceforge*)
+            wget -O ./download "$url/download"
+            unzip -o ./download -d "$PLUGIN_INSTALL_PATH"
+            rm -f ./download
         ;;
-        *) wget -O ./${url##*/} "$url" && unzip -o ./${url##*/} -d ${PLUGIN_INSTALL_PATH}
+        *)
+            archive_name=${url##*/}
+            wget -O "./$archive_name" "$url"
+            unzip -o "./$archive_name" -d "$PLUGIN_INSTALL_PATH"
         ;;
     esac
 done
